79602f4df3c6d47a9e92b3070eb3b75509338257
Kennedy Musau <kennedy.musauu@gmail.com>
2023-06-27 23:34:17 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
+    def name=(name)
+        @name = name
+    end
 
+    def name 
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+    def job
+        @job
+    end
 end
+
+student =Person.new
+student.name = "Kennedy"
+student.job = "Softwere Developer"
+puts student.name
+puts student.job
